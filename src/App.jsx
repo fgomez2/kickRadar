9a144c437386a433a837b0e1001ccd8295e3c0b0
Fer <fergg424@gmail.com>
2025-10-27 22:39:30 +0100

    Creación de rutas y demás

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import KickHeader from './components/KickHeader'
-import KickMain from './components/KickMain'
-import KickFooter from './components/KickFooter'
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './routes/index.jsx'
 import './App.css'
 
 function App() {
   return (
     <>
-      <div className="min-h-screen flex flex-col">
+      {/* <div className="min-h-screen flex flex-col">
         <KickHeader />
         <KickMain />
         <KickFooter />
-      </div>
+      </div> */}
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </>
     
   )
