eea59c7d38f5a7a22d9fee98d940bc58bd454bdd
Fer <fergg424@gmail.com>
2025-11-28 23:58:19 +0100

    feat: En proceso la implementación de la búsqueda de sneakers, creada ruta, hook y componente (NO ESTÁ TODO COMPLETO)

--- src/pages/BusquedaSneakers.jsx
+++ src/pages/BusquedaSneakers.jsx
@@ -1,5 +1,18 @@
+import KickHeader from "../components/KickHeader"
+import { useParams } from "react-router-dom"
+
 export default function BusquedaSneakers() {
+
+    const { busqueda } = useParams()
+
     return (
-        
+        <>
+            <KickHeader />
+            <main className="min-h-screen bg-black text-white pt-6 px-4">
+                <h1 className="text-2xl font-bold mb-4">Resultados para: <span className="text-green-400">"{busqueda}"</span></h1>
+
+                {/* AQUÍ IRA EL GRID DE RESULTADOS MÁS ADELANTE */}
+            </main>
+        </>
     )
 }
