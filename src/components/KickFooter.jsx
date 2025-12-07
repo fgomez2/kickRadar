7ccb343df561dfcf42273b3b876eaff989f55b84
Fer <fergg424@gmail.com>
2025-12-07 19:06:02 +0100

    Mostrar sneakers favoritas del usuario en la página MisFavoritos y poder eliminarlas desde ahí también

--- src/components/KickFooter.jsx
+++ src/components/KickFooter.jsx
@@ -1,6 +1,5 @@
 import '@fontsource/anton'
 import estilosFooter from './KickFooter.module.css'
-import { redireccionStockxLogin } from '../utils/stockxAuth.js'
 
 export default function KickFooter() {
     return (
@@ -8,7 +7,7 @@ export default function KickFooter() {
             <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-32 h-px bg-gradient-to-r from-transparent via-green-400 to-transparent shadow-[0_0_8px_rgba(34,197,94,0.6)]"></div>
             <div className="absolute top-0 left-0 right-0 h-16 bg-gradient-to-t from-black via-gray-900/20 to-transparent pointer-events-none"></div>
             <div className="p-4 relative z-10">
-                <p className="text-sm">&copy; 2024 KickRadar. Todos los derechos reservados.</p>
+                <p className="text-sm">&copy; 2025 KickRadar. Todos los derechos reservados.</p>
             </div>
         </footer>
     )
